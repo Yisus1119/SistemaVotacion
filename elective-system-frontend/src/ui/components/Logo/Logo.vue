f67d20a06b83f1813d49b5bd7bef20c20859c3b4
amfotep <202010603@itla.edu.do>
2023-06-30 02:17:36 -0300

    logo created

---
+++ elective-system-frontend/src/ui/components/Logo/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+	<img
+		src="../../../assets/vectors/votacion logo.png"
+		width="90"
+		height="90"
+		alt="LOGO"
+	/>
+	<p style="text-align: center; font-weight: bold">SDGPVE</p>
+</template>
--- elective-system-frontend/src/ui/pages/MainPage/MainPage.vue
+++ elective-system-frontend/src/ui/pages/MainPage/MainPage.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from "../../components/Logo/Logo.vue";
+</script>
 
 <template>
 	<div class="container-fluid">
 		<div class="div-top-slide d-flex">
-			<img src="" alt="LOGO" />
+			<div>
+				<Logo></Logo>
+			</div>
 			<div class="div-top-slide-column">
 				<p>Bienvenido al nuevo</p>
 				<p>
@@ -11,6 +15,38 @@
 					electorales
 				</p>
 			</div>
+		</div>
+		<hr />
+
+		<div class="div-body">
+			<p>
+				"El voto electrónico: un camino hacia la democracia del siglo
+				XXI, donde la tecnología une a los ciudadanos en una voz
+				electrónica de cambio y empoderamiento."
+			</p>
+			<article class="div-body-logos-container">
+				<div>
+					<img
+						src="../../../assets/vectors/precision.png"
+						alt="IMG"
+					/>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div>
+					<img
+						src="../../../assets/vectors/participacion.png"
+						alt="IMG"
+					/>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div>
+					<img
+						src="../../../assets/vectors/flexibilidad.png"
+						alt="IMG"
+					/>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+			</article>
 		</div>
 	</div>
 </template>
