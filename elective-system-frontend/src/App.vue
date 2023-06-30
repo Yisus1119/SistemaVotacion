1939bbb1b42f48d4180e4fb8ff21b41c63db5981
Yisus1119 <jesusemigenio@gmail.com>
2023-06-30 02:28:35 -0400

    [ADD] A temporal commit

--- elective-system-frontend/src/App.vue
+++ elective-system-frontend/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 //import HelloWorld from "./components/HelloWorld.vue";
-import MainPage from "./ui/pages/MainPage/MainPage.vue";
-//import AboutPage from "./ui/pages/AboutPage/AboutPage.vue";
+// import MainPage from "./ui/pages/MainPage/MainPage.vue";
+import AboutPage from "./ui/pages/AboutPage/AboutPage.vue";
 import Navbar from "./ui/components/Navbar/Navbar.vue";
 </script>
 <template>
 	<Navbar></Navbar>
-	<MainPage></MainPage>
+	<!-- <MainPage></MainPage> -->
+<AboutPage></AboutPage>
 </template>
--- elective-system-frontend/src/ui/pages/AboutPage/AboutPage.vue
+++ elective-system-frontend/src/ui/pages/AboutPage/AboutPage.vue
@@ -1,7 +1,78 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from "../../components/Logo/Logo.vue";
+</script>
 
 <template>
-	<div>
-		<h1>ABOUT PAGE</h1>
+	<div class="container-fluid">
+		<div class="div-tio-slide d-flex">
+			<div>
+				<Logo></Logo>
+			</div>
+			<div class="div-top-slide-column">
+				<div class="titleContainer">
+					<img class="aboutUs" src="../../../assets/vectors/equipo.png" alt="team" />
+
+					<h1 class="principalTitle">¿Quiénes somos?</h1>
+				</div>
+				<p class="aboutDescription"><b>
+						Somos un equipo de dos apasionados desarrolladores que compartimos un objetivo común: solucionar el
+						desafío de la votación en nuestro país.
+						Nos hemos unido para abordar esta problemática con nuestra experiencia en desarrollo de software y
+						nuestro compromiso
+						con la innovación tecnológica. <br><br>
+
+						Nuestro enfoque se basa en la transparencia, la integridad y la accesibilidad, buscando garantizar
+						la
+						participación ciudadana y fortalecer
+						la confianza en el proceso electoral. Nos apasiona encontrar soluciones innovadoras que puedan
+						superar
+						los desafíos actuales y mejorar la
+						experiencia de votación para todos los ciudadanos.</b>
+				</p>
+				<br>
+
+				<div class="titleContainer">
+					<img class="aboutUs" src="../../../assets/vectors/mision.png" alt="team" />
+					<h1 class="principalTitle">Misión</h1>
+				</div>
+				<p class="aboutDescription"><b>
+						Nuestra misión es transformar la forma en que se lleva a cabo la votación en nuestro país, brindando
+						soluciones tecnológicas innovadoras
+						y seguras que promuevan la transparencia, la accesibilidad y la confianza en el proceso electoral.
+						<br>
+						Nos comprometemos a facilitar la participación
+						ciudadana y fortalecer la democracia a través de una plataforma de votación confiable, eficiente y
+						de fácil uso. </b>
+				</p>
+				<br>
+
+				<div class="titleContainer">
+					<img class="aboutUs" src="../../../assets/vectors/vision.png" alt="team" />
+					<h1 class="principalTitle">Visión</h1>
+				</div>
+				<p class="aboutDescription"><b>
+						Nuestra visión es ser líderes en el desarrollo de soluciones tecnológicas para la votación,
+						reconocidos por nuestra excelencia,
+						integridad y compromiso con la mejora continua. Aspiramos a ser la opción preferida tanto para los
+						ciudadanos que desean ejercer
+						su derecho al voto de manera segura y conveniente, como para las autoridades electorales que buscan
+						modernizar y optimizar sus procesos.</b>
+				</p>
+				<br>
+
+				<div class="titleContainer">
+					<img class="aboutUs" src="../../../assets/vectors/valores.png" alt="team" />
+
+					<h1 class="principalTitle">Valores</h1>
+				</div>
+				<br>
+				<img class="values" src="../../../assets/vectors/ValuesList.svg" alt="team" />
+				<br>
+				<br>
+			</div>
+		</div>
 	</div>
 </template>
+
+
+<style scoped src="./style.css"></style>
