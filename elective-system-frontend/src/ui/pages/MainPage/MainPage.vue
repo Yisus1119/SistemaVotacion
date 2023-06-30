58fd79d2248353807cd9807d100438b5920cf19a
amfotep <202010603@itla.edu.do>
2023-06-30 03:15:39 -0300

    main page created

--- elective-system-frontend/src/ui/pages/MainPage/MainPage.vue
+++ elective-system-frontend/src/ui/pages/MainPage/MainPage.vue
@@ -3,7 +3,7 @@ import Logo from "../../components/Logo/Logo.vue";
 </script>
 
 <template>
-	<div class="container-fluid">
+	<div class="container-fluid d-flex flex-column min-vh-100">
 		<div class="div-top-slide d-flex">
 			<div>
 				<Logo></Logo>
@@ -25,29 +25,54 @@ import Logo from "../../components/Logo/Logo.vue";
 				electrónica de cambio y empoderamiento."
 			</p>
 			<article class="div-body-logos-container">
-				<div>
+				<div class="div-body-logos-container-item">
 					<img
-						src="../../../assets/vectors/precision.png"
+						src="../../../assets/vectors/Presition.svg"
 						alt="IMG"
+						width="200"
+						height="200"
 					/>
-					<p>Lorem ipsum dolor sit amet.</p>
+					<p class="div-body-logos-container-item-text">
+						Precision y velocidad
+					</p>
 				</div>
-				<div>
+				<div class="div-body-logos-container-item">
 					<img
-						src="../../../assets/vectors/participacion.png"
+						src="../../../assets/vectors/Participation.svg"
 						alt="IMG"
+						width="200"
+						height="200"
 					/>
-					<p>Lorem ipsum dolor sit amet.</p>
+					<p class="div-body-logos-container-item-text">
+						Mayor participacion
+					</p>
 				</div>
-				<div>
+				<div class="div-body-logos-container-item">
 					<img
-						src="../../../assets/vectors/flexibilidad.png"
+						src="../../../assets/vectors/Flexibility.svg"
 						alt="IMG"
+						width="200"
+						height="200"
 					/>
-					<p>Lorem ipsum dolor sit amet.</p>
+					<p class="div-body-logos-container-item-text">
+						Flexibilidad y adaptabilidad
+					</p>
 				</div>
 			</article>
+
+			<p class="div-body-top-text">
+				Que esperas para unirte a esta revolucion digital?
+			</p>
 		</div>
+
+		<footer
+			style="
+				margin-top: auto;
+				background-color: #98d8aa;
+				width: 100%;
+				height: 80px;
+			"
+		></footer>
 	</div>
 </template>
 
