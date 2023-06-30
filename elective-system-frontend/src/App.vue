f2e9d8286d135f458a66a8d0d0a49cee3e433f81
amfotep <202010603@itla.edu.do>
2023-06-30 01:37:40 -0300

    init main page

--- elective-system-frontend/src/App.vue
+++ elective-system-frontend/src/App.vue
@@ -2,7 +2,9 @@
 //import HelloWorld from "./components/HelloWorld.vue";
 import MainPage from "./ui/pages/MainPage/MainPage.vue";
 //import AboutPage from "./ui/pages/AboutPage/AboutPage.vue";
+import Navbar from "./ui/components/Navbar/Navbar.vue";
 </script>
 <template>
+	<Navbar></Navbar>
 	<MainPage></MainPage>
 </template>
--- elective-system-frontend/src/ui/pages/MainPage/MainPage.vue
+++ elective-system-frontend/src/ui/pages/MainPage/MainPage.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>
-		<h1>MAIN PAGE</h1>
+	<div class="container-fluid">
+		<div class="div-top-slide d-flex">
+			<img src="" alt="LOGO" />
+			<div class="div-top-slide-column">
+				<p>Bienvenido al nuevo</p>
+				<p>
+					Sistema digital de gestion y procesamiento de votos
+					electorales
+				</p>
+			</div>
+		</div>
 	</div>
 </template>
+
+<style scoped src="./styles.css"></style>
