78f431b06a3f4b5661d4d94ad36f99dc66cdee33
amfotep <202010603@itla.edu.do>
2023-08-06 04:38:34 -0300

    [ADDED] results page and styles

--- elective-system-frontend/src/router/index.router.ts
+++ elective-system-frontend/src/router/index.router.ts
@@ -4,6 +4,8 @@ import AboutPage from "../ui/pages/AboutPage/AboutPage.vue";
 import HowItWorkPage from "../ui/pages/HowItWorksPage/HowItWorksPage.vue";
 import LoginUserPageVue from "../ui/pages/LoginUserPage/LoginUserPage.vue";
 import AdminLogin from "../ui/pages/AdminLogin/AdminLogin.vue";
+import ElectionMainPageVue from "../ui/pages/ElectionMainPage/ElectionMainPage.vue";
+import ResultsPageVue from "../ui/pages/ResultsPage/ResultsPage.vue";
 
 const routes: RouteRecordRaw[] = [
 	{
@@ -22,13 +24,21 @@ const routes: RouteRecordRaw[] = [
 		component: HowItWorkPage,
 	},
 	{
-		path:"/admin-login",
+		path: "/admin-login",
 		name: "Sign In as admin",
-		component: AdminLogin
+		component: AdminLogin,
 	},
-	{ path: "/login", 
-	name: "Login User", 
-	component: LoginUserPageVue },
+	{ path: "/login", name: "Login User", component: LoginUserPageVue },
+	{
+		path: "/election-main",
+		name: "Election Main",
+		component: ElectionMainPageVue,
+	},
+	{
+		path: "/results",
+		name: "Results",
+		component: ResultsPageVue,
+	},
 ];
 
 const router = createRouter({
